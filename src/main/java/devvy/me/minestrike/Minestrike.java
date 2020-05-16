5124361cec9d609ca5048df537bbe794a437a53e
Courteouss <curtiskampf@gmail.com>
2020-05-16 00:55:26 -0400

    Update Minestrike.java
    plugins

--- src/main/java/devvy/me/minestrike/Minestrike.java
+++ src/main/java/devvy/me/minestrike/Minestrike.java
@@ -1,12 +1,17 @@
 package devvy.me.minestrike;
 
+import devvy.me.minestrike.Play.KillDeath;
+import devvy.me.minestrike.Play.Scoreboard;
+import devvy.me.minestrike.Play.Teams;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Minestrike extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+       this.getServer().getPluginManager().registerEvents(new Teams(), this);
+       this.getServer().getPluginManager().registerEvents(new KillDeath(), this);
+       //this.getServer().getPluginManager().registerEvents(new Scoreboard(), this);
 
     }
 
