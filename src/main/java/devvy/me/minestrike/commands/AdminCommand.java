5d0e1541c1a9d4d8e2dbddcdc90d5068832dff00
Devvy <devinhall4@gmail.com>
2020-05-17 05:10:42 -0400

    more functionality for the admin command for debugging

--- src/main/java/devvy/me/minestrike/commands/AdminCommand.java
+++ src/main/java/devvy/me/minestrike/commands/AdminCommand.java
@@ -1,11 +1,15 @@
 package devvy.me.minestrike.commands;
 
+import devvy.me.minestrike.game.CSTeam;
 import devvy.me.minestrike.game.GameManager;
+import devvy.me.minestrike.game.TeamType;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,7 +26,7 @@ public class AdminCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        String[] subs = {"start", "end", "next", "status"};
+        String[] subs = {"start", "end", "next", "status", "swap", "spectate"};
         if (command.getName().equalsIgnoreCase("admin")){
             if (args.length == 1){
                 ArrayList<String> options = new ArrayList<>();
@@ -51,8 +55,10 @@ public class AdminCommand implements CommandExecutor, TabCompleter {
             return false;
         }
 
+        String action = args[0].toUpperCase();
+
         // What should we do?
-        switch (args[0].toUpperCase()){
+        switch (action){
 
             case "START":
                 handleStartSubcommand(sender);
@@ -66,6 +72,21 @@ public class AdminCommand implements CommandExecutor, TabCompleter {
                 handleNextSubcommand(sender);
                 break;
 
+            case "SWAP":
+
+                if (sender instanceof Player)
+                    handleSwapSubcommand((Player)sender);
+                else
+                    sender.sendMessage(ChatColor.RED + "Only players can do this!");
+                break;
+
+            case "SPECTATE":
+                if (sender instanceof Player)
+                    handleSpectateSubcommand((Player)sender);
+                else
+                    sender.sendMessage(ChatColor.RED + "Only players can do this!");
+                break;
+
             case "STATUS":
                 handleStatusSubcommand(sender);
                 break;
@@ -76,6 +97,45 @@ public class AdminCommand implements CommandExecutor, TabCompleter {
         }
 
         return true;
+
+    }
+
+    private void handleSpectateSubcommand(Player player) {
+
+        CSTeam currTeam = gameManager.getTeamManager().getPlayerTeam(player);
+
+        if (currTeam.getType() == TeamType.SPECTATORS){
+            player.sendMessage(ChatColor.RED + "You are already spectating!");
+            return;
+        }
+
+        currTeam.removeMember(player);  // Remove them from whatever team they are on
+        gameManager.getTeamManager().getSpectators().addMember(player);  // Add them to spectators
+        player.setGameMode(GameMode.SPECTATOR);
+        player.sendMessage(ChatColor.DARK_GRAY + "You are now a spectator");
+
+
+    }
+
+    private void handleSwapSubcommand(Player player) {
+
+        CSTeam currTeam = gameManager.getTeamManager().getPlayerTeam(player);
+        CSTeam t = gameManager.getTeamManager().getAttackers();
+        CSTeam ct = gameManager.getTeamManager().getDefenders();
+
+        // Remove them from their current team and set them to adv just in case
+        currTeam.removeMember(player);
+        player.setGameMode(GameMode.ADVENTURE);
+
+        // If they are spectating or a ct, set them to be a t
+        if (currTeam.getType() == TeamType.SPECTATORS || currTeam == ct){
+            t.addMember(player);
+            player.sendMessage(ChatColor.GOLD + "You are now a T");
+        // Must be a t, make them a ct
+        } else {
+            ct.addMember(player);
+            player.sendMessage(ChatColor.BLUE + "You are now a CT");
+        }
 
     }
 
