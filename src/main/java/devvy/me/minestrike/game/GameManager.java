f4aab51807a0af7a71176b89ccbd59c553631972
Devvy <devinhall4@gmail.com>
2020-06-09 06:24:02 -0400

    cleanup

--- src/main/java/devvy/me/minestrike/game/GameManager.java
+++ src/main/java/devvy/me/minestrike/game/GameManager.java
@@ -19,7 +19,6 @@ public class GameManager implements Listener {
     private final RoundManager roundManager;
     private final TeamManager teamManager;
     private final PlayerManager playerManager;
-    //private final TabList tabList;
     private final Sidebar scoreboardManager;
 
     public GameManager(Minestrike plugin) {
@@ -28,14 +27,12 @@ public class GameManager implements Listener {
         roundManager = new RoundManager(plugin);
         teamManager = new TeamManager();
         playerManager = new PlayerManager(plugin);
-        //tabList = new TabList();
         scoreboardManager = new Sidebar();
 
 
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
         plugin.getServer().getPluginManager().registerEvents(teamManager, plugin);
         plugin.getServer().getPluginManager().registerEvents(playerManager, plugin);
-       // plugin.getServer().getPluginManager().registerEvents(tabList, plugin);
         plugin.getServer().getPluginManager().registerEvents(scoreboardManager, plugin);
     }
     public void initializeTabList(){
